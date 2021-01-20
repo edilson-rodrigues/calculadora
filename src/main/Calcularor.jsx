ab45cde770dd811eeeaba6a4ab369cf89dbf9ddf
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-20 12:01:56 -0300

    implementando a logica #02

--- src/main/Calcularor.jsx
+++ src/main/Calcularor.jsx
@@ -22,14 +22,55 @@ class Calculator extends Component {
 		this.addDigit = this.addDigit.bind(this);
 	}
 
+	//limpa o visor
 	clearMemory() {
 		this.setState({ ...initialState });
 	}
 
+	//operações
 	setOperation(operation) {
-		console.log(operation);
+		if (this.state.current === 0) {
+			this.setState({ operation, current: 1, clearDisplay: true });
+		} else {
+			const equals = operation === "=";
+			const currentOperation = this.state.operation;
+
+			const values = [...this.state.values];
+
+			try {
+				switch (currentOperation) {
+					case "-":
+						values[0] = values[0] - values[1];
+						break;
+					case "+":
+						values[0] = values[0] + values[1];
+						break;
+					case "/":
+						values[0] = values[0] / values[1];
+						break;
+					case "*":
+						values[0] = values[0] * values[1];
+						break;
+					default:
+						values[0] = this.state.values[0];
+				}
+			} catch (error) {
+				values[0] = this.state.values[0];
+			}
+
+			values[1] = 0;
+
+			this.setState({
+				displayValue: values[0],
+				operation: equals ? null : operation,
+				current: equals ? 0 : 1,
+				clearDisplay: !equals,
+				values,
+			});
+		}
 	}
 
+	// adiciona os digitos
 	addDigit(n) {
 		if (n === "." && this.state.displayValue.includes(".")) {
 			return;

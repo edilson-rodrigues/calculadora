f78457993f5b3803009d9f0ce1bf047a9fa78eaa
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-20 10:22:51 -0300

    componente button #02

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,20 @@
 import "./Button.css";
 
 const Button = (props) => {
-	const { label } = props;
-	return <button className="button">{label}</button>;
+	const { label, operation, double, triple, click } = props;
+
+	let classes = "button ";
+	classes += operation ? "operation" : "";
+	classes += double ? "double" : "";
+	classes += triple ? "triple" : "";
+
+	const clicked = () => click && click(label);
+
+	return (
+		<button onClick={clicked} className={classes}>
+			{label}
+		</button>
+	);
 };
 
 export default Button;
